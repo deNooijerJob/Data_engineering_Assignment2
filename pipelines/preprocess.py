509182bf658a57a6b379ef86d7ffc387d71a8032
Job de Nooijer <j.n.d.nooijer@student.tue.nl>
2020-11-02 13:24:02 +0100

    working preprocessing

--- pipelines/preprocess.py
+++ pipelines/preprocess.py
@@ -1,12 +1,10 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
 import argparse
 import logging
 import sys
 import json
-
 import apache_beam as beam
 from apache_beam.metrics.metric import Metrics
 from apache_beam.options.pipeline_options import GoogleCloudOptions
@@ -14,14 +12,18 @@ from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
 from apache_beam.options.pipeline_options import StandardOptions
 from apache_beam.transforms import trigger
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import re
 
 '''
     writes the tweets to a bigquery database
 '''
+
+
 class WriteToBigQuery(beam.PTransform):
-
     def __init__(self, table_name, dataset, schema, project):
-
         beam.PTransform.__init__(self)
         self.table_name = table_name
         self.dataset = dataset
@@ -29,7 +31,6 @@ class WriteToBigQuery(beam.PTransform):
         self.project = project
 
     def get_schema(self):
-
         return ', '.join('%s:%s' % (col, self.schema[col]) for col in self.schema)
 
     def expand(self, pcoll):
@@ -44,10 +45,16 @@ class WriteToBigQuery(beam.PTransform):
 
 '''
     Initial preprocessing
-    
+
     takes the input from pubsub as json and only keeps the usefull fields
 '''
+
+
 class ParseTweet(beam.DoFn):
+    nltk.download('stopwords')
+    stop_words = stopwords.words("english")
+    stemmer = SnowballStemmer("english")
+    invalid_chars = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
 
     def __init__(self):
         beam.DoFn.__init__(self)
@@ -55,12 +62,26 @@ class ParseTweet(beam.DoFn):
 
     def process(self, elem):
         try:
-            item = json.loads(elem) # reads the element
+            item = json.loads(elem)  # reads the element
+
+            stem = False
+            text = re.sub(self.invalid_chars, ' ',
+                          str(item['text']).lower()).strip()  # Remove link,user and special characters
+            tokens = []
+            for token in text.split():
+                if token not in self.stop_words:
+                    if stem:
+                        tokens.append(self.stemmer.stem(token))
+                    else:
+                        tokens.append(token)
+            pp_tweet = " ".join(tokens)
+
             yield {
+                'tweet': pp_tweet,
                 'user_id': item['user_id'],
-                'tweet': item['text'],
-                'timestamp': 12345 # TODO fix the timestamp
+                'time_stamp': 1234567  # time_stamp not implemented
             }
+
         except:  # pylint: disable=bare-except
             # Log and count parse errors
             self.num_parse_errors.inc()
@@ -70,6 +91,8 @@ class ParseTweet(beam.DoFn):
 '''
     Extract the tweets from input stream
 '''
+
+
 class ExtractTweets(beam.PTransform):
 
     def __init__(self, field):
@@ -81,9 +104,13 @@ class ExtractTweets(beam.PTransform):
                 pcoll
                 | beam.Map(lambda elem: (elem[self.field], elem['tweet']))
         )
+
+
 '''
     get the initial tweets from the tweetstsream
 '''
+
+
 class GetTweets(beam.PTransform):
     def __init__(self, allowed_lateness):
         beam.PTransform.__init__(self)
@@ -91,24 +118,26 @@ class GetTweets(beam.PTransform):
 
     def expand(self, pcoll):
         return (
-            pcoll
-            | 'TweetGlobalWindows' >> beam.WindowInto(
+                pcoll
+                | 'TweetGlobalWindows' >> beam.WindowInto(
             beam.window.GlobalWindows(),
-            trigger=trigger.Repeatedly(trigger.AfterCount(10)),
+            trigger=trigger.Repeatedly(trigger.AfterCount(50)),
             accumulation_mode=trigger.AccumulationMode.ACCUMULATING,
             allowed_lateness=self.allowed_lateness_seconds)
-            # Extract and sum username/score pairs from the event data.
-            | 'ExtractTweets' >> ExtractTweets('user_id')
+                # Extract and sum username/score pairs from the event data.
+                | 'ExtractTweets' >> ExtractTweets('user_id')
         )
+
 
 '''
     main run loop
 '''
+
+
 def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
-
     parser.add_argument('--topic', type=str, help='Pub/Sub topic to read from')
-   
+
     parser.add_argument(
         '--subscription', type=str, help='Pub/Sub subscription to read from')
     parser.add_argument(
@@ -129,60 +158,53 @@ def run(argv=None, save_main_session=True):
     parser.add_argument(
         '--table_name')
     args, pipeline_args = parser.parse_known_args(argv)
-
     if args.topic is None and args.subscription is None:
         parser.print_usage()
         print(sys.argv[0] + ': error: one of --topic or --subscription is required')
         sys.exit(1)
-
     options = PipelineOptions(pipeline_args)
 
     # We use the save_main_session option because one or more DoFn's in this
     # workflow rely on global context (e.g., a module imported at module level).
     options.view_as(SetupOptions).save_main_session = save_main_session
-
     # Enforce that this pipeline is always run in streaming mode
     options.view_as(StandardOptions).streaming = True
-
     with beam.Pipeline(options=options) as p:
-
         # Read from PubSub into a PCollection.
         if args.subscription:
             tweets = p | 'ReadPubSub' >> beam.io.ReadFromPubSub(
                 subscription=args.subscription)
         else:
             tweets = p | 'ReadPubSub' >> beam.io.ReadFromPubSub(topic=args.topic)
-
         '''
             first steps in the pipline
         '''
         out_tweets = (
                 tweets
-                | 'DecodeString' >> beam.Map(lambda b: b.decode('utf-8')) # make sure that the tweets are in utf-8 base
-                | 'ParseTweets' >> beam.ParDo(ParseTweet()) # parse all the tweets
-                # TODO add another pre processing step ParDo??
-                # TODO train the model
-                | 'AddTimestamps' >> beam.Map(
-                    lambda elem: beam.window.TimestampedValue(elem, elem['timestamp'])) # add timestamps (why???)
+                | 'DecodeString' >> beam.Map(lambda b: b.decode('utf-8'))  # make sure that the tweets are in utf-8 base
+                | 'ParseTweets' >> beam.ParDo(ParseTweet())  # parse all the tweets
+            # TODO train the model
+
         )
 
         def format_tweets(tw):
-            (tweet, user_id) = tw
-            return {'user_id': user_id, 'tweet': tweet}
+            (user_id, tweet) = tw
+            return {'user_id': user_id, 'tweet': tweet}  # timestamp not implemented
 
         # Write to Bigquery
         (
-            out_tweets
-            | 'getTweets' >> GetTweets(args.allowed_lateness)  # get the tweets
-            | 'format output' >> beam.Map(format_tweets) # format the tweets
-            | 'store twitter posts' >> WriteToBigQuery( # write them to the db
-                args.table_name + '_tweets',
-                args.dataset, {
-                    'user_id': 'STRING',
-                    'tweet': 'STRING',
-                },
-                options.view_as(GoogleCloudOptions).project)
+                out_tweets
+                | 'getTweets' >> GetTweets(args.allowed_lateness)  # get the tweets
+                | 'format output' >> beam.Map(format_tweets)  # format the tweets
+                | 'store twitter posts' >> WriteToBigQuery(  # write them to the db
+            args.table_name + '_tweets',
+            args.dataset, {
+                'tweet': 'STRING',
+                'user_id': 'STRING'  # timestamp not implemented
+            },
+            options.view_as(GoogleCloudOptions).project)
         )
+
 
 '''
     run the pipeline
@@ -190,3 +212,7 @@ def run(argv=None, save_main_session=True):
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
     run()
+
+
+
+
