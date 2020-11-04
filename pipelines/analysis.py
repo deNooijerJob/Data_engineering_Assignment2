ed74c56f7063944f4e5135de29d21fec4c3749a3
mscheve <mscheve@gmail.com>
2020-11-04 14:10:51 +0000

    working analysis + viz

--- pipelines/analysis.py
+++ pipelines/analysis.py
@@ -1,3 +1,4 @@
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -7,6 +8,10 @@ import logging
 import time
 import sys
 import json
+import io
+import urllib, base64
+import numpy as np
+from matplotlib import pyplot as plt
 
 import apache_beam as beam
 from apache_beam.metrics.metric import Metrics
@@ -63,19 +68,56 @@ def sentimentAnalysis (project_id, bucket_name, name, tweets):
     tokenizer = unpickle()
 
     score = 0
-    logging.info(tweets)
     for tweet in tweets:  # tweets {useris : job, tweet: text}
-        logging.info(tweet)
         # Tokenize text
-        x_test = pad_sequences(tokenizer.texts_to_sequences([tweet['text']]), maxlen=300)
+        x_test = pad_sequences(tokenizer.texts_to_sequences([tweet]), maxlen=300)
         # Predict
         score += model.predict([x_test])[0]
-        # Decode sentiment
 
     avg_score = score / len(tweets)
-    label = decode_sentiment(avg_score, include_neutral=True)
+    label = decode_sentiment(avg_score, include_neutral=True) #decode sentiment
+    return [{"Name": name, "sentiment": label, "Score": float(avg_score)}]
 
-    return ['{"Name": %s, "sentiment": %s, "Score": %0.3f}' % (name, label, float(avg_score))]
+def survey(bucket_name, pre_results):
+    logging.info(pre_results)
+    results = {'Sentiment': [pre_results[0]['Score'], pre_results[1]['Score']]}
+    category_names = [pre_results[0]['Name'], pre_results[1]['Name']]
+    labels = list(results.keys())
+    data = np.array(list(results.values()))
+    data_cum = data.cumsum(axis=1)
+    category_colors = plt.get_cmap('RdBu_r')(
+        np.linspace(0.15, 0.85, data.shape[1]))
+
+    fig, ax = plt.subplots(figsize=(9.2, 5))
+    ax.invert_yaxis()
+    ax.xaxis.set_visible(False)
+    ax.set_xlim(0, np.sum(data, axis=1).max())
+
+    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+        widths = data[:, i]
+        starts = data_cum[:, i] - widths
+        ax.barh(labels, widths, left=starts, height=0.5,
+                label=colname, color=color)
+        xcenters = starts + widths / 2
+
+        text_color = 'black'
+        for y, (x, c) in enumerate(zip(xcenters, widths)):
+            ax.text(x, y, str(c), ha='center', va='center',
+                    color=text_color)
+    ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1),
+              loc='lower left', fontsize='small')
+    
+    #fig = plt.figure()
+    #fig_to_upload = plt.gcf()
+    
+    #buf = io.BytesIO()
+    plt.savefig('result.png')
+    #buf.seek(0)
+    #image_as_a_string = base64.b64encode(buf.read())
+    client = storage.Client()
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob('results/result.png')
+    contents = blob.upload_from_filename('result.png')
 
 def run ( argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
@@ -94,8 +136,8 @@ def run ( argv=None, save_main_session=True):
 
     pipeline_options = PipelineOptions(
         flags=pipeline_args,
-        project='data-engeneering-289509',
-        temp_location='gs://data_engineering2020/tmp/',
+        project='data-engineering2020',
+        temp_location='gs://data_engineering_2020/tmp/',
         region='europe-west4')
 
     pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
@@ -103,43 +145,50 @@ def run ( argv=None, save_main_session=True):
     with beam.Pipeline(options=pipeline_options) as p:
         trump_tweets = (
             p
-            | 'Query trump tweets' >> beam.io.Read(beam.io.BigQuerySource(
-                query='SELECT `tweet` FROM `data-engeneering-289509.tweetdata.trump`',
+            | 'Query Trump tweets' >> beam.io.Read(beam.io.BigQuerySource(
+                query='SELECT `tweet` FROM `data-engineering2020.tweet_data.trump`',
                 use_standard_sql=True))
-	    | beam.Map(lambda elem: elem['tweet'])
+	    | 'ExtractTweetsTrump' >> beam.Map(lambda elem: elem['tweet'])
         )
 
         trump_sentiment = (
             p
-            | 'init trump list' >> beam.Create([])
-            | 'Analyse sentiment trump' >> beam.FlatMap(
+            | 'GetPID Trump' >> beam.Create([known_args.pid])
+            | 'Analyse sentiment Trump' >> beam.FlatMap(
                 sentimentAnalysis,
-                project_id=known_args.pid,
-                mbucket=known_args.mbucket,
+                bucket_name=known_args.mbucket,
                 name="Trump",
                 tweets=beam.pvalue.AsList(trump_tweets))
         )
-
+        
         biden_tweets = (
             p
-            | 'Query biden tweets' >> beam.io.Read(beam.io.BigQuerySource(
-                query='SELECT `tweet`  FROM `data-engeneering-289509.tweetdata.biden`',
+            | 'Query Biden tweets' >> beam.io.Read(beam.io.BigQuerySource(
+                query='SELECT `tweet`  FROM `data-engineering2020.tweet_data.biden`',
                 use_standard_sql=True))
-            | beam.Map(lambda elem: elem['tweet'])
+            | 'ExtractTweetsBiden' >> beam.Map(lambda elem: elem['tweet'])
         )
 
         biden_sentiment = (
             p
-            | 'init biden list' >> beam.Create([])
-            | 'Analyse sentiment biden' >> beam.FlatMap(
+            | 'GetProjectID Biden' >> beam.Create([known_args.pid])
+            | 'Analyse sentiment Biden' >> beam.FlatMap(
                 sentimentAnalysis,
-                project_id=known_args.pid,
-                mbucket=known_args.mbucket,
+                bucket_name=known_args.mbucket,
                 name="Biden",
                 tweets=beam.pvalue.AsList(biden_tweets))
         )
+        
+        composed_result = (
+            (trump_sentiment, biden_sentiment)
+            | 'Merge sentiments' >> beam.Flatten()
+        )
 
-        composed_result = ((trump_sentiment, biden_sentiment) | 'Merge sentiments' >> beam.Flatten())
+        (
+            p
+            | 'GetBucketName' >> beam.Create([known_args.mbucket])
+            | 'Create Visualization' >> beam.FlatMap(survey, pre_results=beam.pvalue.AsList(composed_result))
+        )
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
